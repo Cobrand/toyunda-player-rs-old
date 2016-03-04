3876132345de1586266fa2489b3229ac64dec091
Yamakaky <yamakaky@yamaworld.fr>
2016-03-04 22:48:08 +0100

    Add docopt.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
+extern crate rustc_serialize;
+extern crate docopt;
+
+extern crate libc;
+
 #[macro_use]
 extern crate enum_primitive;
-extern crate docopt;
-extern crate libc;
 extern crate num;
+
 
 mod mpv;
 
+const USAGE: &'static str = "
+toyunda-player.
+
+Usage:
+  toyunda-player [options] <file>
+  toyunda-player -h | --help
+  toyunda-player --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --invert      Invert the screen.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct CmdArgs {
+    flag_invert: bool,
+    arg_file: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: CmdArgs = docopt::Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
