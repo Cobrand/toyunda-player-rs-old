66512efbc6d21d7e0b726cfa520f819e5e5ef545
n3f4s <tiv.crb@gmail.com>
2016-03-09 10:04:37 +0100

    implementation of Event_Dispatcher

--- src/event_dispatcher.rs
+++ src/event_dispatcher.rs
@@ -1,30 +1,34 @@
 use std::collections::HashMap;
 
 use sdl2::event::Event;
+use std::intrinsics;
 
-//type Event = sdl2::Event;
-type Callback = Box<(Fn(&mut Event) -> Result<(), String>)>;
+type Callback = Box<(Fn(&Event) -> Result<(), String>)>;
 
-//fn mk_callback<F>(f: &F) -> Callback
-//    where F: Fn(&mut Event) -> Result<(), i32> {
-//        Box::new(*f) as Callback
-//    }
+#[feature(core_intrinsics)]
+pub fn get_type<T>(_: &T) -> String {
+    unsafe { intrinsics::type_name::<T>() }.into()
+}
+
+pub struct Event_Type {
+    evt: Event,
+}
 
 pub struct Event_Dispatcher {
-    events: HashMap<Event, Callback>,
+    events: HashMap<String, Callback>,
 }
 
 impl Event_Dispatcher {
     pub fn register<F>(&mut self, id: Event, fun: Callback) {
-            self.events.insert(id, fun);
+        self.events.insert(&get_type(&id), fun);
     }
-    pub fn handle(&self, evt: Event) -> Result<(), String>{
-        match self.events.get(evt) {
+    pub fn handle(&self, evt: Event) -> Result<(), String> {
+        match self.events.get(get_type(&evt)) {
             Some(&fun) => {
-                fun(evt);
+                fun(&evt);
                 Ok(())
-            },
-            None => Err(format!("Event {:?} not handled", evt))
+            }
+            None => Err(format!("Event {:?} not handled", evt)),
         }
     }
 }
