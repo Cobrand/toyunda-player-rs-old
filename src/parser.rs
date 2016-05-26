8960f9991cca8701b3c5782672b7a99f8dd3ee38
n3f4s <tiv.crb@gmail.com>
2016-05-26 13:36:13 +0200

    add doc to types

--- src/parser.rs
+++ src/parser.rs
@@ -3,12 +3,18 @@
 mod subtitle {
     use std::vec::Vec;
 
+    /**
+     * Coordinate of a point
+     */
     #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Default)]
     pub struct Position {
         x: i8,
         y: i8,
     }
 
+    /**
+     * RGB color
+     */
     #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Default)]
     pub struct Color {
         r: i8,
@@ -16,21 +22,33 @@ mod subtitle {
         b: i8,
     }
 
+    /**
+     * Smallest piece of a text
+     */
     #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
     pub struct Syllable {
-        color: Color,
         pos: Position,
         word: String,
     }
 
-    type FrameNb = usize;
+    /// Frame stamp
+    pub type FrameNb = usize;
 
+    /**
+     * Pair of FrameNb and type T. Used to
+     * associate something with the first frame when it appears.
+     */
     #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
-    struct Pair<T> {
+    pub struct Pair<T> {
         pub frame: FrameNb,
         pub value: T,
     }
 
+    /**
+     * String printed on the screen.
+     * All the string appears, or none of it.
+     * Contains a "pointer" to the last Syllable that changed color
+     */
     #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
     pub struct Sentence {
         last_colored: usize,
@@ -38,6 +56,9 @@ mod subtitle {
         pub syllables: Vec<Pair<Syllable>>,
     }
 
+    /**
+     * Subtitle for all the video
+     */
     #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
     pub struct Sub {
         current_frame: usize,
